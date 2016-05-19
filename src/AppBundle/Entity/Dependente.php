6a859288fcb1f958ac12b74aea143baf27743ec1
Held Grijo <shadyueh@gmail.com>
2016-05-19 00:50:59 -0400

    Entities generated

---
+++ src/AppBundle/Entity/Dependente.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dependente
+ *
+ * @ORM\Table(name="dependente")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\DependenteRepository")
+ */
+class Dependente
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", length=255)
+     */
+    private $nome;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="data_nascimento", type="date")
+     */
+    private $dataNascimento;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="parentesco", type="string", length=255)
+     */
+    private $parentesco;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     *
+     * @return Dependente
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set dataNascimento
+     *
+     * @param \DateTime $dataNascimento
+     *
+     * @return Dependente
+     */
+    public function setDataNascimento($dataNascimento)
+    {
+        $this->dataNascimento = $dataNascimento;
+
+        return $this;
+    }
+
+    /**
+     * Get dataNascimento
+     *
+     * @return \DateTime
+     */
+    public function getDataNascimento()
+    {
+        return $this->dataNascimento;
+    }
+
+    /**
+     * Set parentesco
+     *
+     * @param string $parentesco
+     *
+     * @return Dependente
+     */
+    public function setParentesco($parentesco)
+    {
+        $this->parentesco = $parentesco;
+
+        return $this;
+    }
+
+    /**
+     * Get parentesco
+     *
+     * @return string
+     */
+    public function getParentesco()
+    {
+        return $this->parentesco;
+    }
+}
+
---
+++ src/AppBundle/Entity/Funcionario.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Funcionario
+ *
+ * @ORM\Table(name="funcionario")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FuncionarioRepository")
+ */
+class Funcionario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", length=255)
+     */
+    private $nome;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cpf", type="string", length=255)
+     */
+    private $cpf;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="data_nascimento", type="date")
+     */
+    private $dataNascimento;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="endereco", type="string", length=255)
+     */
+    private $endereco;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     *
+     * @return Funcionario
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set cpf
+     *
+     * @param string $cpf
+     *
+     * @return Funcionario
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    /**
+     * Get cpf
+     *
+     * @return string
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * Set dataNascimento
+     *
+     * @param \DateTime $dataNascimento
+     *
+     * @return Funcionario
+     */
+    public function setDataNascimento($dataNascimento)
+    {
+        $this->dataNascimento = $dataNascimento;
+
+        return $this;
+    }
+
+    /**
+     * Get dataNascimento
+     *
+     * @return \DateTime
+     */
+    public function getDataNascimento()
+    {
+        return $this->dataNascimento;
+    }
+
+    /**
+     * Set endereco
+     *
+     * @param string $endereco
+     *
+     * @return Funcionario
+     */
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+    /**
+     * Get endereco
+     *
+     * @return string
+     */
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+}
+
